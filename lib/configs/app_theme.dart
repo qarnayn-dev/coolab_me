7ec1b629eb1bc7af741243f9c03d44c62c993392
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2023-02-19 22:02:41 +0800

    UP – theme data + app_root init

--- lib/configs/app_theme.dart
+++ lib/configs/app_theme.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Color(0xFFFFD4B2);
+  static const int _primaryHex = 0xFFFFD4B2;
+  // static const Color primary = Color(_primaryHex);
+
+  static const MaterialColor primary = MaterialColor(
+    _primaryHex,
+    <int, Color>{
+      50: Color(0xffffead9),
+      100: Color(0xffffe5d1),
+      200: Color(0xffffe1c9),
+      300: Color(0xffffddc1),
+      400: Color(0xffffd8ba),
+      500: Color(_primaryHex),
+      600: Color(0xffe6bfa0),
+      700: Color(0xffccaa8e),
+      800: Color(0xffb3947d),
+      900: Color(0xff997f6b),
+    },
+  );
+
   static const Color primaryS2 = Color(0xFFF3B27F);
   static const Color primaryS3 = Color(0xFFEB914B);
   static const Color secondary = Color(0xFFFFF6BD);
---
+++ lib/routing/app_root.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// The root of the application.
+// TODO: Update the navigation logic here.
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
