d99f496d6363d33348cd62808bcc59ade9c180ca
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2023-02-19 21:42:47 +0800

    ADD – theme colors

---
+++ lib/configs/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xFFFFD4B2);
+  static const Color primaryS2 = Color(0xFFF3B27F);
+  static const Color primaryS3 = Color(0xFFEB914B);
+  static const Color secondary = Color(0xFFFFF6BD);
+  static const Color accent = Color(0xFFCEEDC7);
+  static const Color accent2 = Color(0xFF86C8BC);
+  static const Color nBlack = Color(0xFF242424);
+  static const Color nGrey = Color(0xFFA0A0A0);
+  static const Color nWhite = Color(0xFFEAEAEA);
+
+  static const Map<int, Color> labelColor = {
+    1: Color(0xFF39B5E0),
+    2: Color(0xFF59CE8F),
+    3: Color(0xFFEB455F),
+    4: Color(0xFF7D1E6A),
+    5: Color(0xFF24A19C),
+    6: Color(0xFF8E0505),
+    7: Color(0xFF292C6D),
+    8: Color(0xFF1C7947),
+    9: Color(0xFFEE5007),
+    10: Color(0xFFFFC947),
+  };
+}
